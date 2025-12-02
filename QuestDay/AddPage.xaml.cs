2302c0818c8c448c6f59e83d80de1c32b89ba716
Violetta233 <Wiolettalebed5@gmail.com>
2025-12-02 19:51:46 +0300

    Added home screen + habit addition screen

---
+++ QuestDay/AddPage.xaml.cs
@@ -0,0 +1,17 @@
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;  // Äëÿ IValueConverter
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System.Windows.Input;
+namespace QuestDay
+{
+    public partial class AddPage : ContentPage
+    {
+        public AddPage()
+        {
+            InitializeComponent();
+        } 
+    }
+}
