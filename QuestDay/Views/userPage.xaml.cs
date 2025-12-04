1bb81820008709639f3f2aa782dcf1678a44b121
ansavaa <annasavockina78@gmail.com>
2025-12-04 18:15:16 +0300

    Добавлен кролик на страницу гардероба

--- QuestDay/Views/userPage.xaml.cs
+++ QuestDay/Views/userPage.xaml.cs
@@ -1,13 +1,29 @@
+using QuestDay.Views;
 using System.Diagnostics;
-using QuestDay.ViewModels;
 
 namespace QuestDay.Views
 {
-	public partial class userPage : ContentPage
-	{
-		public userPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class userPage : ContentPage
+    {
+        public userPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnHatButtonClicked(object sender, EventArgs e)
+        {
+            // Например: Navigation.PushAsync(new HatSelectionPage());
+            Debug.WriteLine("Hat button clicked");
+        }
+
+        private void OnTopButtonClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Top button clicked");
+        }
+
+        private void OnBottomButtonClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Bottom button clicked");
+        }
+    }
 }
