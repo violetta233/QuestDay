414099dea955b76bcdbcc96123c4d8a8c783f07b
anastasia0756 <anastasiavolk95@gmail.com>
2026-03-17 11:37:59 +0300

    fix "Add Page" screen

--- QuestDay/Views/AddPage.xaml.cs
+++ QuestDay/Views/AddPage.xaml.cs
@@ -20,5 +20,18 @@ namespace QuestDay.Views
                 viewModel.DaysOfWeekSelection.Reset();
             }
         }
+        private async void OnNavClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var button = sender as ImageButton;
+                string route = button.CommandParameter.ToString();
+                await Shell.Current.GoToAsync($"//{route}");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ошибка", $"Не удалось открыть страницу: {ex.Message}", "ОК");
+            }
+        }
     }
 }
