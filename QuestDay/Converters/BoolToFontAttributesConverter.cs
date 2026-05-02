41c68eea150164d53c70f3e1fc6317672282bacc
Violetta233 <Wiolettalebed5@gmail.com>
2026-05-03 00:50:46 +0300

    adding converter for calendar

---
+++ QuestDay/Converters/BoolToFontAttributesConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace QuestDay.Converters
+{
+    public class BoolToFontAttributesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isToday && isToday)
+            {
+                return FontAttributes.Bold;
+            }
+            return FontAttributes.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QuestDay/Converters/IsPositiveConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace QuestDay.Converters
+{
+    public class IsPositiveConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int number)
+            {
+                return number > 0;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
